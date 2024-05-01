568839e67a670494ccb89479c64b1ddff2c3eb52
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-05-01 12:02:53 +0300

    create navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,14 +2,19 @@ import { Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
 import Basket from "../pages/Basket"
 import NotFound from "../pages/NotFound"
+import Navbar from "../components/Navbar"
 
 const AppRouter = () => {
   return (
+    <>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="basket" element={<Basket/>}/>
       <Route path="*" element={<NotFound/>}/>
     </Routes>
+    </>
+    
   )
 }
 
